d19e899488c23e31310f0e8f100ed6ad54b89c2d
Jared <jaredsims398@gmail.com>
2024-09-16 19:18:18 -0600

    cleaned up

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -84,11 +84,15 @@ public class ChessBoard {
 
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ChessBoard that = (ChessBoard) o;
-        return Arrays.deepEquals(this.squares, that.squares);
+    public boolean equals(Object check) {
+        if (this == check)
+            return true;
+        if (check == null || getClass() != check.getClass())
+            return false;
+
+        ChessBoard board = (ChessBoard) check;
+
+        return Arrays.deepEquals(this.squares, board.squares);
     }
 
     @Override
