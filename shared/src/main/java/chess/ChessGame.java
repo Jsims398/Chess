03ecc46ee5b5d72daa6cbc5802d24e841928c9bf
Jared <jaredsims398@gmail.com>
2024-09-30 21:11:33 -0600

    valid moves logic

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,8 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -10,24 +12,29 @@ import java.util.Collection;
  */
 public class ChessGame {
 
-    public ChessGame() {
+    private TeamColor turn;
+    private ChessBoard board;
+    private boolean gameStatus;
 
+    public ChessGame() {
+        board = new ChessBoard();
+        setTeamTurn(TeamColor.WHITE);
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return turn;
     }
 
     /**
-     * Set's which teams turn it is
+     * Set's which teams turn it is-
      *
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        turn = team;
     }
 
     /**
@@ -35,7 +42,16 @@ public class ChessGame {
      */
     public enum TeamColor {
         WHITE,
-        BLACK
+        BLACK;
+
+        public String toString(){
+            if(this == WHITE){
+                return "white";
+            }
+            else{
+                return "black";
+            }
+        }
     }
 
     /**
@@ -46,7 +62,27 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+        ChessPiece current = board.getPiece(startPosition);
+        if(current == null){
+            return null;
+        }
+        HashSet<ChessMove> p_moves = (HashSet<ChessMove>) board.getPiece(startPosition).pieceMoves(board, startPosition);
+        HashSet<ChessMove> v_moves = HashSet.newHashSet(p_moves.size());
+
+        for(ChessMove move : p_moves){
+            ChessPiece tempPiece = board.getPiece(move.getEndPosition());
+            board.addPiece(startPosition, null);//removes piece in start spot
+            board.addPiece(move.getEndPosition(), current);
+
+            if (!isInCheck(current.getTeamColor())) {
+                v_moves.add(move);
+            }
+
+            board.addPiece(move.getEndPosition(), tempPiece);
+            board.addPiece(startPosition, current);
+        }
+
+        return v_moves;
     }
 
     /**
@@ -106,5 +142,27 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         throw new RuntimeException("Not implemented");
+    }
+
+
+    @Override
+    public String toString() {
+        return "ChessGame{" +
+                "teamTurn=" + turn +
+                ", board=" + board +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessGame chessGame = (ChessGame) o;
+        return turn == chessGame.turn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(turn, board);
     }
 }
