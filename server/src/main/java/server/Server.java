d94deaaac2c73952c9e09ec5bed13f057c2763ca
Jared <jaredsims398@gmail.com>
2024-10-19 21:25:46 -0600

    UserHandler set up need to test

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -37,10 +37,19 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::clear);
+        Spark.post("/user", userHandler::register);
+        Spark.post("/session", userHandler::login);
+        Spark.delete("/session", userHandler::logout);
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.get("/game", gameHandler::listGames);
+        Spark.post("/game", gameHandler::createGame);
+        Spark.put("/game", gameHandler::joinGame);
+
+        Spark.exception(BadRequestException.class, this::badRequestExceptionHandler);
+        Spark.exception(UnauthorizedException.class, this::unauthorizedExceptionHandler);
+        Spark.exception(Exception.class, this::genericExceptionHandler);
+
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -49,5 +58,29 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object clear(Request req, Response resp) {
+
+        userService.clear();
+        gameService.clear();
+
+        resp.status(200);
+        return "{}";
+    }
+//handle exceptions
+    private void badRequestExceptionHandler(BadRequestException ex, Request req, Response resp) {
+        resp.status(400);
+        resp.body("{ \"message\": \"Error: bad request\" }");
+    }
+
+    private void unauthorizedExceptionHandler(UnauthorizedException ex, Request req, Response resp) {
+        resp.status(401);
+        resp.body("{ \"message\": \"Error: unauthorized\" }");
+    }
+
+    private void genericExceptionHandler(Exception ex, Request req, Response resp) {
+        resp.status(500);
+        resp.body("{ \"message\": \"Error: %s\" }".formatted(ex.getMessage()));
     }
 }
--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,6 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.*;
 import service.UserService;
+import model.*;
+import spark.*;
 
 public class UserHandler {
     UserService userService;
@@ -8,4 +12,37 @@ public class UserHandler {
     public UserHandler(UserService userService) {
         this.userService = userService;
     }
+
+    //register
+    public Object register(Request request, Response response) throws BadRequestException {
+        UserData user = new Gson().fromJson(request.body(), UserData.class);
+
+        if (user.username() == null || user.password() == null) {
+            throw new BadRequestException("No username or password");
+        }
+        try {
+            AuthData authData = userService.createUser(user);
+            response.status(200);
+            return new Gson().toJson(authData);
+        } catch (BadRequestException s) {
+            response.status(403);
+            return "{message: Error: already taken}";
+        }
+    }
+
+    //login
+    public Object login(Request request, Response response) throws UnauthorizedException, BadRequestException {
+        UserData user = new Gson().fromJson(request.body(), UserData.class);
+        AuthData auth = userService.login(user);
+        response.status(200);
+        return new Gson().toJson(auth);
+    }
+    //logout
+    public Object logout(Request request, Response responce) throws UnauthorizedException{
+        String auth = request.headers("authorization");
+        userService.logoutUser(auth);
+        responce.status(200);
+        return "{}";
+    }
 }
+
